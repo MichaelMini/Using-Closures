f904f9a8c11d2be5180193a0053d05f8bcaf0451
MichaelMini <mcho911@gmail.com>
2017-01-13 03:15:27 +0000

    Finial

--- countdown.js
+++ countdown.js
@@ -1,5 +1,26 @@
 var countdownGenerator = function (x) {
   /* your code here */
+  var messages = ['T-minus ', 'Blast Off!', 'Rockets already gone, bub!'];
+  var initCount = x;
+  return function() {
+  	if ( initCount > 0 ) {
+  		var message = messages[0] + initCount + '...';
+	  	console.log(message);
+	  	initCount -= 1;
+	  	return message;
+  	} else if ( initCount === 0 ) {
+  		var message = messages[1];
+	  	console.log(message);
+	  	initCount -= 1;
+	  	return message;
+  	} else if ( initCount < 0 ) {
+  		var message = messages[2];
+	  	console.log(message);
+	  	initCount -= 1;
+	  	return message;
+  	}
+  	initCount -= 1;
+  }
 };
 
 var countdown = countdownGenerator(3);
